a3e06c847facb4e85d75e1ef6adf9b7aab714d5a
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-11-08 20:10:13 -0600

    added Header background and styling to the nav bar

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import tachyons from "tachyons";
+
+export default function Header() {
+  return (
+    <header className="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+      <nav className=" f6 fw6 ttu tracked">
+        <a className="link dim white dib mr3" href="#" title="Home">
+          Home
+        </a>
+        <a className="link dim white dib mr3" href="#" title="About">
+          About
+        </a>
+        <a className="link dim white dib" href="#" title="Contact">
+          Contact
+        </a>
+      </nav>
+    </header>
+  );
+}
